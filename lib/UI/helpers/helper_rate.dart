386669c42565d3ef524d0c4e2028da8562c429b9
Samiul Basir <samiulbasirbhuiyan1234@gmail.com>
2024-07-27 23:17:41 +0600

    Add new helper rate option

---
+++ lib/UI/helpers/helper_rate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HelperRate extends StatefulWidget {
+  const HelperRate({super.key});
+
+  @override
+  State<HelperRate> createState() => _HelperRateState();
+}
+
+class _HelperRateState extends State<HelperRate> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
